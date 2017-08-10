be0b831c63f7c6eb50ffdc87aeb00982f63f00b5
VINOD <vinodkumar.u93@gmail.com>
2017-08-10 22:34:00 +0800

    upload final source code

---
+++ NYP.AzureAD/NYP.AzureAD/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NYP.AzureAD.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        
+        public IHttpActionResult GetUserName()
+        {
+            try
+            {
+                return Ok(User.Identity.Name);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
